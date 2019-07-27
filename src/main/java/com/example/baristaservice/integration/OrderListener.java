687b129a81170f10058d41b41dd3885f510d61be
I337593 <kevin.jin01@sap.com>
2019-07-27 16:28:04 +0800

    Use kafka as MQ

--- src/main/java/com/example/baristaservice/integration/OrderListener.java
+++ src/main/java/com/example/baristaservice/integration/OrderListener.java
@@ -5,11 +5,9 @@ import com.example.baristaservice.model.OrderState;
 import com.example.baristaservice.repository.CoffeeOrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
@@ -23,19 +21,16 @@ public class OrderListener {
   private CoffeeOrderRepository orderRepository;
   @Autowired
   private Waiter waiter;
-  @Autowired
-  @Qualifier(Waiter.FINISHED_ORDERS)
-  private MessageChannel finishedOrderMessageChannel;
-
   @Value("${order.barista-prefix}${random.uuid}")
   private String barista;
 
   @StreamListener(Waiter.NEW_ORDERS)
-  public void processNewOrder(Long id) {
+  @SendTo(Waiter.FINISHED_ORDERS)
+  public Long processNewOrder(Long id) {
     CoffeeOrder order = orderRepository.getOne(id);
     if (order == null) {
       log.warn("Order id {} is NOT valid.", id);
-      return;
+      throw new IllegalArgumentException("Order id is NOT valid.");
     }
 
     log.info("Receive new order {}. Waiter: {}. Customer: {}",
@@ -44,9 +39,7 @@ public class OrderListener {
     order.setBarista(barista);
     orderRepository.save(order);
     log.info("Order {} is READY.", order);
-
-//    waiter.finishedOrders().send(MessageBuilder.withPayload(order.getId()).build());
-    finishedOrderMessageChannel.send(MessageBuilder.withPayload(order.getId()).build());
+    return order.getId();
   }
 
 }
